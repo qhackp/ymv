bc5148ab19a7cdbdc5234cbe815ef346b4a58384
qhackp <qhackp@naver.com>
2015-06-08 16:46:10 +0900

    자 여러분 ymv 프로젝트 시작입니다. 설정파일들 확인해보시고 수정사항 있으면 수정하시구요 삭제는 절대 하지 마십시오. 삭제할게 생기면 단톡방에 남기고 한명이 삭제업무 담당합시다.

---
+++ ymv/src/main/java/org/log5j/ymv/controller/TestController.java
@@ -0,0 +1,12 @@
+package org.log5j.ymv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	@RequestMapping("home.ymv")
+	public String test(){
+		return "index";
+	}
+}
